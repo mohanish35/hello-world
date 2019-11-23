888a24ec6a254485c8057543dbcb2563196e0035
Mohanish Mankar <mohanishmankar@Mohanishs-MacBook-Air.local>
2019-11-23 22:21:48 +0530

    Initial Vue app.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.use(express.static('my-app/dist'))
+
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/my-app/dist/index.html');
