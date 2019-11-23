c0c2047c544dd7ab306d099235fa2a87458f60c0
Mohanish Mankar <mohanishmankar@Mohanishs-MacBook-Air.local>
2019-11-23 22:18:40 +0530

    Initial Vue app.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/my-app/dist/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
